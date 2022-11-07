76c23a47e7036430f38dcf2c1765a54e859c1206
hpekkan <75019129+hpekkan@users.noreply.github.com>
2022-11-08 00:18:05 +0300

    SQL UPDATE

---
+++ insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO KULLANICI VALUES ('1','Ata','TURHAN','aturhan@gmail.com','54312369','sogutozu','aturhan','1234',0);
+INSERT INTO KULLANICI VALUES ('2','Huseyin','PEKKAN','hpekkan@gmail.com','54312331','eryaman','hpekkan','4321',1);
+INSERT INTO KART VALUES ('1','1234123412341234','123','yemek');
+INSERT INTO KART VALUES ('2','4321432143214321','321','kiyafet');
+INSERT INTO ODEME VALUES ('1','kart',100,'2020-08-10','protein tozu');
+INSERT INTO ODEME VALUES ('2','kart',3155,'2020-08-15','testosteron');
+INSERT INTO URUN VALUES (null,'https://m.media-amazon.com/images/I/61ItnQ7-g7L._AC_SX679_.jpg','ofis',5,'Faber Kastil','3');
+INSERT INTO URUN VALUES (null,'https://cdn.cimri.io/image/1200x1200/rotringmekanikkurunversatilkalemfiyatlar_317185202.jpg','ofis',50,'Rotrink','1');
+INSERT INTO URUN VALUES (null,'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/129876-2-2_large.jpg','telefon',20000,'Ayfon','2');
+
+INSERT INTO MAGAZA VALUES ('1','hepsiburada','proteinci','spor');
+INSERT INTO MAGAZA VALUES ('2','proteinocean','testo','takviye-gida');
+INSERT INTO URUN VALUES (null,'https://sneakerbaker.com/wp-content/uploads/2022/01/New-Balance-530-Steel-Grey-600x428.png','spor',45,'Nike','1');
+INSERT INTO URUN VALUES (null,'https://www.thenextsole.com/storage/images/369451-01.png','spor',60,'New Balance','2');
+INSERT INTO SEPET VALUES ('1');
+INSERT INTO SEPET VALUES ('2');
+INSERT INTO SIPARIS VALUES ('1','aylik protein');
+INSERT INTO SIPARIS VALUES ('2','aylik dogal vitamin');
+INSERT INTO EKLENIR VALUES ('1','1',1);
+INSERT INTO EKLENIR VALUES ('2','2',2);
+INSERT INTO BULUNUR VALUES ('1','1');
+INSERT INTO BULUNUR VALUES ('2','2');
+INSERT INTO OLUSTURUR VALUES ('1','1');
+INSERT INTO OLUSTURUR VALUES ('2','2');
+INSERT INTO SAHIPTIR VALUES ('1','1');
+INSERT INTO SAHIPTIR VALUES ('2','2');
+INSERT INTO SATILIR  VALUES ('1','1',1);
+INSERT INTO SATILIR  VALUES ('2','2',1);
+INSERT INTO TAMAMLANIR VALUES ('1','1');
+INSERT INTO TAMAMLANIR VALUES ('2','2');
+INSERT INTO VERIR VALUES ('1','1');
+INSERT INTO VERIR VALUES ('2','2');
+INSERT INTO YAPAR VALUES ('1','1');
+INSERT INTO YAPAR VALUES ('2','2');
+INSERT INTO YAPILIR VALUES ('1','1');
+INSERT INTO YAPILIR VALUES ('2','2');
+
+
+
+
